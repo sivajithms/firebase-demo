ade0472a9799a93a6aea36a65fd96cb5a2e07f77
sivajithms <sivajithm@gmail.com>
2023-03-19 01:48:30 +0530

    data reflected on the page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import './App.css';
-import {firebaseApp} from './firebase/configs'
-import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
+import "./App.css";
+import { firebaseApp } from "./firebase/configs";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { useState, useEffect } from "react";
+import Table from 'react-bootstrap/Table';
 
 const db = getFirestore(firebaseApp);
 
-const q = query(collection(db, "products"));
-
 function App() {
+  const [products, setProducts] = useState([]);
+
+
+    const q = query(collection(db, "products"));
+    getDocs(q).then((querySnapshot) => {
+      const data = querySnapshot.docs.map((doc) => doc.data());
+      setProducts(data);
+    });
+ 
+
   return (
     <div className="App">
-      <button onClick={async()=>{
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
-        });
-
-      }}>click me</button>
+      <div>
+        <Table  striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.name}>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 }
