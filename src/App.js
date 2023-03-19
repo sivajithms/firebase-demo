adc9204c6e663f6b236fdec82d058540f18fddb9
sivajithms <sivajithm@gmail.com>
2023-03-19 13:56:42 +0530

    data added to db

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import "./App.css";
 import { firebaseApp } from "./firebase/configs";
+
 import {
   getFirestore,
   collection,
   query,
   where,
   getDocs,
+  addDoc
 } from "firebase/firestore";
+
+import { getDatabase, ref, set } from "firebase/database";
 import { useState, useEffect } from "react";
 import Table from 'react-bootstrap/Table';
 
 const db = getFirestore(firebaseApp);
+const userCollectionRef = collection(db,'products')
 
 function App() {
   const [products, setProducts] = useState([]);
 
+  const q = query(collection(db, "products"));
+  getDocs(q).then((querySnapshot) => {
+    const data = querySnapshot.docs.map((doc) => doc.data());
+    setProducts(data);
+  });
 
-    const q = query(collection(db, "products"));
-    getDocs(q).then((querySnapshot) => {
-      const data = querySnapshot.docs.map((doc) => doc.data());
-      setProducts(data);
+  function writeUserData() {
+    addDoc(userCollectionRef, {
+      name: "mii",
+      price: "890"
     });
- 
+  }
 
   return (
     <div className="App">
       <div>
-        <Table  striped bordered hover variant="dark">
+        <Table striped bordered hover variant="dark">
           <thead>
             <tr>
               <th>name</th>
@@ -43,6 +53,7 @@ function App() {
           </tbody>
         </Table>
       </div>
+      <button onClick={writeUserData}>click me</button>
     </div>
   );
 }
